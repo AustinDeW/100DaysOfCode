646c13b98121f20f3299fba55b2ea805fec40fd6
Austin <austinrdewitt1@gmail.com>
2017-01-14 14:53:20 -0700

    Added comments to Form1.cs

--- C#/Reminder/Reminder/Form1.cs
+++ C#/Reminder/Reminder/Form1.cs
@@ -3,27 +3,30 @@ using System.Windows.Forms;
 using System.IO;
 using System.Collections.Generic;
 using System.Configuration;
-
-//TODO: Add comments to make easier to understand
 namespace Reminder
 {
     public partial class Form1 : Form
     {
         EmailHandler em = null;
-        string path = @ConfigurationManager.AppSettings["ReminderFileLocation"];
-        int exitTime = 5;
+        string path = @ConfigurationManager.AppSettings["ReminderFileLocation"]; // location of file that contains reminders
+        int exitTime = 5; // time that application will auto exit
         public Form1()
         {
             InitializeComponent();
-            tbReminderName.KeyDown += (sender, e) => KeyDown_SubmitReminder(sender, e);
+            tbReminderName.KeyDown += (sender, e) => KeyDown_SubmitReminder(sender, e); // Allows 'enter' key to submit 
 
             try { em = new EmailHandler(); }
             catch (Exception ex) { Console.WriteLine("\n\n" + ex.Message + "\n\n" + ex.StackTrace); }
 
-            timerExit.Start();
+            timerExit.Start(); // start auto exit countdown timer
             CheckForReminder();
         }
 
+        /// <summary>
+        /// Adds a reminder to the file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAddReminder_Click(object sender, EventArgs e)
         {
             string reminderName = tbReminderName.Text;
@@ -34,31 +37,36 @@ namespace Reminder
             FileHandler.WriteFile(path, strReminder);
         }
 
+        /// <summary>
+        /// Checks the reminder file to see if there are any reminders within the time frame
+        /// </summary>
         private void CheckForReminder()
         {
             try
             {
                 if (File.Exists(path))
                 {
-                    List<string> liMonthlyReminder = new List<string>();
-                    string[] sReminders = FileHandler.ReadFile(path);
+                    List<string> liMonthlyReminder = new List<string>(); // List that contains reminders that need to have date updated
+                    string[] sReminders = FileHandler.ReadFile(path); // All of the reminders from the reminder file
                     string sReminder = "";
                     string sDate = "";
                     bool bSendEmail = false;
                     for (int i = 0; i < sReminders.Length; ++i)
                     {
+                        // Reminder date
                         string date = sReminders[i].Substring(0, sReminders[i].IndexOf('-'));
 
                         DateTime dt = Convert.ToDateTime(date);
 
                         string format = "MM/dd/yyyy";
 
+                        //TODO: Allow user to specify time frame to send reminder
                         if (dt.ToString(format) == DateTime.Today.ToString(format) 
-                                ||  dt.ToString(format) == DateTime.Today.AddDays(3).ToString(format))
+                                ||  dt.ToString(format) == DateTime.Today.AddDays(3).ToString(format)) // Checks if a reminder is within the time frame
                         {
                             sReminder += sReminders[i].Substring(sReminders[i].IndexOf('-') + 1) + " | ";
 
-                            if(sReminders[i].EndsWith("*"))
+                            if(sReminders[i].EndsWith("*")) // '*' signifies a reminder that needs to be auto updated
                             {
                                 liMonthlyReminder.Add(sReminders[i]);
                             }
@@ -98,12 +106,23 @@ namespace Reminder
             }
         }
 
+        /// <summary>
+        /// If checkbox is checked it will stop application from auto exiting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cbStopExit_CheckedChanged(object sender, EventArgs e)
         {
             Console.WriteLine("Stopped Exiting.\n\n");
             timerExit.Stop();
         }
 
+
+        /// <summary>
+        /// KeyDown event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void KeyDown_SubmitReminder(object sender, KeyEventArgs e)
         {
             switch(e.KeyCode)
