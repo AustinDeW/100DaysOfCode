c0dbaf449b2b52a519953a2395212c1c8b1c41db
Austin <austinrdewitt1@gmail.com>
2017-01-15 14:14:28 -0700

    added comments to fileHandler.cs

--- C#/Reminder/Reminder/FileHandler.cs
+++ C#/Reminder/Reminder/FileHandler.cs
@@ -6,19 +6,31 @@ using System.Text;
 
 namespace Reminder
 {
-    //TODO: Add comments to FileHandler.cs
     static class FileHandler
     {   
+        /// <summary>
+        /// Writes a string to the file that contains the reminders
+        /// </summary>
+        /// <param name="contents">String to be written</param>
         public static void WriteFile(string contents)
         {
             File.AppendAllText(ConfigurationManager.AppSettings["ReminderFileLocation"], contents);
         }
 
+        /// <summary>
+        /// Reads in the reminder file specified in App.config
+        /// </summary>
+        /// <returns>String array filled with the reminders</returns>
         public static string[] ReadFile()
         {
             return File.ReadAllLines(ConfigurationManager.AppSettings["ReminderFileLocation"]);
         }
 
+        /// <summary>
+        /// Reads in a specific file
+        /// </summary>
+        /// <param name="path">File to be read</param>
+        /// <returns>String array filled with contents from given file</returns>
         public static string[] ReadFile(string path)
         {
             return File.ReadAllLines(path);
@@ -26,13 +38,18 @@ namespace Reminder
 
 
         //TODO: Specify update renewal period, such as Month or Year or Day
+        /// <summary>
+        /// Updates a reminders date
+        /// </summary>
+        /// <param name="liMonthlyReminders">List containing reminders that need to be updated</param>
+        /// <param name="sReminders">Array of all of the reminders in the file</param>
         public static void UpdateRenewalDate(List<string> liMonthlyReminders, string[] sReminders)
         {
             for (int i = 0; i < sReminders.Length; i++)
             {
                 for (int j = 0; j < liMonthlyReminders.Count; j++)
                 {
-                    if (liMonthlyReminders[j] == sReminders[i])
+                    if (liMonthlyReminders[j] == sReminders[i]) // updates the reminders date
                     {
                         DateTime dtNextMonth = Convert.ToDateTime(Utilities.GetDateFromReminder(sReminders[i])).AddMonths(1);
                         sReminders[i] = dtNextMonth.ToString(Utilities.DATE_FORMAT) + "-" + Utilities.GetReminderTitleFromReminder(sReminders[i]);
